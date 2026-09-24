2ba5063059fe13022ef30ddcb4dc816bb6b1ccf1
timusss0 <mustikavia86@gmail.com>
2026-09-24 10:50:46 +0700

    change file package

--- api/tickets.js
+++ api/tickets.js
@@ -131,8 +131,12 @@ export default async function handler(req, res) {
         res.setHeader('Allow', 'GET, POST, PATCH, DELETE');
         return res.status(405).json({ error: 'Method not supported.' });
     }
-  } catch (err) {
+   } catch (err) {
     console.error(err);
-    return res.status(500).json({ error: 'Failed to access the database. Check the DB_* environment variables and make sure the table has been created.' });
+    return res.status(500).json({
+      error: 'Failed to access the database.',
+      code: err.code,
+      detail: err.message,
+    });
   }
 }
